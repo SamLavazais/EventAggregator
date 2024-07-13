2fa856b6096c302b90fee32847286de376967679
SamSam <samuel.lavazais@hotmail.fr>
2024-07-13 15:15:53 +0200

    add one api call function

--- webscraping/scraping.py
+++ webscraping/scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-# from webscraping.date_parser import date_parser
+from webscraping.date_parser import date_parser
 
 
 def scrape_has():
@@ -153,30 +153,28 @@ def scrape_hdh():
 
 
 def scrape_drees():
-    return "blue"
+    url = "https://drees.solidarites-sante.gouv.fr/recherche"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    section = soup.find("ul", class_="search-result")
+    events = section.find_all(
+        "li",
+        class_="search-result-item"
+    )
 
-    # url = "https://drees.solidarites-sante.gouv.fr/recherche"
-    # page = requests.get(url)
-    # soup = BeautifulSoup(page.content, "html.parser")
-    # section = soup.find("ul", class_="search-result")
-    # events = section.find_all(
-    #     "li",
-    #     class_="search-result-item"
-    # )
-    #
-    # data = []
-    # for event in events:
-    #     title = event.find("h3", class_="search-result-item--title").get_text().strip() + " (" + event.find("ul", class_="search-result-item--collection").find_all("li")[0].get_text().strip() + ")"
-    #     date = date_parser(event.find("time").get_text().strip(), "DREES")
-    #     event_url = "https://drees.solidarites-sante.gouv.fr" + event.find("h3", class_="search-result-item--title").find("a")["href"]
-    #     data.append({
-    #         "title": title,
-    #         "date": date,
-    #         "url": event_url,
-    #         "source": "DREES"
-    #     })
-    #
-    # return data
+    data = []
+    for event in events:
+        title = event.find("h3", class_="search-result-item--title").get_text().strip() + " (" + event.find("ul", class_="search-result-item--collection").find_all("li")[0].get_text().strip() + ")"
+        date = date_parser(event.find("time").get_text().strip(), "DREES")
+        event_url = "https://drees.solidarites-sante.gouv.fr" + event.find("h3", class_="search-result-item--title").find("a")["href"]
+        data.append({
+            "title": title,
+            "date": date,
+            "url": event_url,
+            "source": "DREES"
+        })
+
+    return data
 
     # METHODE pour chaque website :
     # définir l'URL
